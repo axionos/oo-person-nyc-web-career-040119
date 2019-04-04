52ddf5c3233a9301bfb08041150839cf789d0e84
github username <github email address>
2019-04-04 13:18:04 -0400

    updated with take_bath method

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,54 @@
 # your code goes here
+class Person
+  attr_accessor :happiness, :bank_account, :hygiene
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def person
+    @person.name
+  end
+
+  def happiness=(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
+
+  def happy?
+    if @happiness > 7
+      true
+    else
+      false
+    end
+  end
+
+  def clean?
+    if @hygiene > 7
+      true
+    else
+      false
+    end
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    @hygiene += 4
+  end
+
+end
